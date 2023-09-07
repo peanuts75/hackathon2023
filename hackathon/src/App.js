8579a71bd13474b865e573e0d08b41df5bb8cd1a
Isaac <isaacbjohnson01@gmail.com>
2023-09-07 16:38:30 +0100

    added logo and fit mannequin

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <div className={'logo'}>
+              <img src={require("./MicrosoftTeams-image (9).png")} alt='logo' className={'logo'} />
+          </div>
         <div className={"container"}>
           <Mannequin></Mannequin>
         </div>
