9e65d3638d4f9534945abf7d200f5edf5f3ed2d8
Jack Rabbit <jack.rabbit@nbrown.co.uk>
2023-09-07 16:12:58 +0100

    updated the mannequin to use an imagemap, can be used for initiating modals

--- hackathon/src/components/Mannequin.js
+++ hackathon/src/components/Mannequin.js
@@ -3,7 +3,13 @@ import '../App.css';
 function Mannequin() {
     return (
         <div id={"mannequin"}>
-            mannequin goes here
+            <img src={require("./Mannequin.png")} useMap="#imagemap" alt={"mannequin"}/>
+            <map name="imagemap">
+                <area target="" alt="top" title="top" href="#top" coords="358,366,95,218" shape="rect"/>
+                <area target="" alt="mid" title="mid" href="#mid" coords="96,367,370,523" shape="rect"/>
+                <area target="" alt="bottom" title="bottom" href="#bottom" coords="106,523,385,990" shape="rect"/>
+                <area target="" alt="shoes" title="shoes" href="#shoes" coords="199,991,272,1128" shape="rect"/>
+            </map>
         </div>
     );
 }
