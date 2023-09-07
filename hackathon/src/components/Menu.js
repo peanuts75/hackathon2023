08188634b958ce39d4c7accfb1eaa7ae2ac43887
Jack Rabbit <jack.rabbit@nbrown.co.uk>
2023-09-07 14:06:07 +0100

    added stuff?

--- hackathon/src/components/Menu.js
+++ hackathon/src/components/Menu.js
@@ -4,19 +4,30 @@ function Menu() {
     return (
         <div id={"menu"}>
             <div className={"menu-item"} id={"dress-menu"}>
+                    Dresses
             </div>
             <div className={"menu-item"} id={"tops-menu"}>
+                    Tops
             </div>
             <div className={"menu-item"} id={"bottoms-menu"}>
+                    Bottoms
             </div>
             <div className={"menu-item"} id={"accessories-menu"}>
+                    Accessories
             </div>
             <div className={"menu-item"} id={"shoes-menu"}>
+                    Shoes
             </div>
             <div className={"menu-item"} id={"total"}>
+                   £0.00 to buy this outfit
             </div>
             <div className={"menu-item"} id={"checkout"}>
+                    Buy it now!
             </div>
+            <div className={"menu-item"} id={"collapse"}>
+                    Collapse menu
+            </div>
+
         </div>
     )
 
