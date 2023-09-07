78c940f29d8e3015df1909c0eb17f4a75821818c
Jack Rabbit <jack.rabbit@nbrown.co.uk>
2023-09-07 11:43:40 +0100

    added mannequin placeholder css/js to page

---
+++ hackathon/src/components/Mannequin.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Mannequin() {
+    return (
+        <div id={"mannequin"}>
+            mannequin goes here
+        </div>
+    );
+}
+
+export default Mannequin;
