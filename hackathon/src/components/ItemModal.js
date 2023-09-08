7743d968ab6f1d491277c454a60c71ad56d46b78
Jack Rabbit <jack.rabbit@nbrown.co.uk>
2023-09-08 14:19:36 +0100

    Updated the styling for the add to cart modal

--- hackathon/src/components/ItemModal.js
+++ hackathon/src/components/ItemModal.js
@@ -1,19 +1,23 @@
 import '../App.css';
 
 function ItemModal() {
-    let name = "generic shirt"
+    let name = "Generic dress"
     let price = "£15.00"
-    let size = ["small", "medium", "large"]
+    let size = ["Small", "Medium", "Large"]
     let total = "£35.00"
     let image = require("../clothes/Dresses/z02fj718010s 14-06-2023_Batch 1 of 1.jpg")
     let imageDimensions = [100, 100, 100, 100]
     return (
         <div id={"item-modal"} className={"addToBag"}>
             <div className={"item-modal-item"} id={"item-modal-name"}>{name}</div>
-            <div className={"item-modal-item"} id={"item-modal-item-price"}>{price}</div>
-            <div className={"item-modal-item"} id={"item-modal-size-dropdown"}>{size.at(1)}</div>
-            <div className={"item-modal-item"} id={"item-modal-add-to-cart"}>Add to cart placeholder button, total at {total}</div>
-            <div className={"item-modal-item"} id={"item-modal-image"}><img src={image} alt={"alt text"} style={{left:imageDimensions.at(0), top:imageDimensions.at(1), width:imageDimensions.at(2), height:imageDimensions.at(3)}}/></div>
+            <div className={"container"}>
+                <div className={"item-modal-item"} id={"item-modal-item-price"}>{price}</div>
+                <div className={"item-modal-item"} id={"item-modal-size-dropdown"}>{size.at(1)}</div>
+            </div>
+            <div className={"container"}>
+                <div className={"item-modal-item"}>Click here to add to cart</div>
+                <div className={"item-modal-item"}>Your current total is {total}</div>
+            </div>
         </div>
     )
 }
